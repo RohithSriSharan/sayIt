fea8c2f70e3c787a3734a57f931e949d893fa0e4
RohithSriSharan <rohithsrishran16@gmail.com>
2023-08-14 02:22:59 +0100

    first commit

---
+++ app/(auth)/onboarding/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const page = () => {
+  return (
+    <div className='flex justify-center items-center text-light-1 h-screen text-body-bold'>
+
+        OnBoarding Coming Soon....
+    </div>
+  )
+}
+
+export default page
---
+++ components/shared/RightSide.tsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Rightside = () => {
+  return (
+    <section className='custom-scrollbar rightsidebar'>
+      <div className='flex flex-1 flex-col justify-start'>
+        <h3 className='text-heading4-medim text-light-1'>Suggested communities</h3>
+      </div>
+      <div className='flex flex-1 flex-col justify-start'>
+        <h3 className='text-heading4-medim text-light-1'>Suggested Users</h3>
+      </div>
+
+    </section>
+  )
+}
+
+export default Rightside
